687492ea1a833a0717efe41bf683fc7cbb86180a
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2022-06-27 04:50:50 -0500

    Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Character from './components/Character';
 function App () {
   return <div className='bg-dark text-white'>
     <h1 className='text-center display-1 py-4'>Rick <span className='text-danger'>&</span> Morty</h1>
+    <h2 className="text-center"> <span className='text-danger'>Todos</span> los <span className='text-primary'>Personajes</span></h2>
     <CharacterList/>
 
   </div>
